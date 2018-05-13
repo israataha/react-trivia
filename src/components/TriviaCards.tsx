a3bc28a7d35cc03ac620f67749152ee4e4becfb5
Israa Taha <taha@cartegraph.com>
2018-05-13 12:37:05 -0500

    Added Next button to iterate through questions

--- src/components/TriviaCards.tsx
+++ src/components/TriviaCards.tsx
@@ -1,3 +1,4 @@
+import {Button} from '@blueprintjs/core';
 import * as React from 'react';
 import { TriviaQuestion } from '../models/TriviaQuestion';
 import TriviaCard from './TriviaCard';
@@ -5,9 +6,23 @@ import TriviaCard from './TriviaCard';
 interface TriviaCardsProps {
     trivia: TriviaQuestion[];
 }
-class TriviaCards extends React.Component<TriviaCardsProps> {
+class TriviaCards extends React.Component<TriviaCardsProps, {currentQuestion: number}> {
+    constructor(props: TriviaCardsProps) {
+        super(props);
+        this.state = {
+            currentQuestion: 0
+        }
+    }
+    public nextQuestion = () => {
+        this.setState({currentQuestion: this.state.currentQuestion+1});
+    }
     public render() {
-        return(<TriviaCard question={this.props.trivia[0]}/>);
+        return(
+            <div>
+                <TriviaCard question={this.props.trivia[this.state.currentQuestion]}/>
+                <Button text="Next" onClick={this.nextQuestion}/>
+            </div>            
+        );
     }
 }
 
