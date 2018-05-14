9b4934e37d46ad25df0c304ae8ae05c0f4128f98
Israa Taha <taha@cartegraph.com>
2018-05-13 22:25:16 -0500

    Indicate whether the selected answer is correct

--- src/components/TriviaCard.tsx
+++ src/components/TriviaCard.tsx
@@ -5,33 +5,35 @@ import { TriviaQuestion } from '../models/TriviaQuestion';
 interface TriviaCardProps {
     question: TriviaQuestion
 }
-
-class TriviaCard extends React.Component<TriviaCardProps> {
-    public answers = this.randomizedAnswers();
-    public handleClick(answer: string) {
-        if (answer === this.props.question.correct_answer) {
-            alert("correct!");
+interface TriviaCardState {
+    selectedAnswer: string
+}
+class TriviaCard extends React.Component<TriviaCardProps, TriviaCardState> {
+    // public answers = this.randomizedAnswers();
+    constructor(props: TriviaCardProps) {
+        super(props);
+        this.state = {
+            selectedAnswer: ''
         }
-        else {
-            alert("wrong!");
-        }
-        
     }
-    public randomizedAnswers(): string[] {
+    public handleClick(answer: string) {
+        this.setState({selectedAnswer: answer});
+    }
+    /*public randomizedAnswers(): string[] {
         const randomizedList = this.props.question.incorrect_answers;
         // Generate a random number between 0 and 2 to insert the correct answer into
         const index = Math.floor(Math.random()*3);
         randomizedList.splice(index, 0, this.props.question.correct_answer);
         return randomizedList;
-    }
+    }*/
     public render() {
         return(
         <Card className="trivia-card">
             <p className="trivia-question">{this.props.question.question}</p>
             {this.props.question.incorrect_answers.map((answer, i)=> {
-                return(<p className="trivia-answer" key={i} onClick={this.handleClick.bind(this, answer)}>{answer}</p>);
+                return(<p className={this.state.selectedAnswer === answer ? 'wrong-answer trivia-answer': 'trivia-answer'} key={i} onClick={this.handleClick.bind(this, answer)}>{answer}</p>);
             })}
-            <p className="trivia-answer" onClick={this.handleClick.bind(this, this.props.question.correct_answer)}>{this.props.question.correct_answer}</p>
+            <p className={this.state.selectedAnswer === this.props.question.correct_answer ? 'correct-answer trivia-answer': 'trivia-answer'} onClick={this.handleClick.bind(this, this.props.question.correct_answer)}>{this.props.question.correct_answer}</p>
         </Card>);
     }
 }
